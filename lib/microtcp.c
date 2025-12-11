946951630e186cd9d8ce3ad2858e0fcbc87214c6
csd5384 <grige@ektoras.localdomain>
2025-12-11 13:21:19 +0200

    createsocket and connect() done

--- lib/microtcp.c
+++ lib/microtcp.c
@@ -24,7 +24,33 @@
 microtcp_sock_t
 microtcp_socket (int domain, int type, int protocol)
 {
-  /* Your code here */
+  /*First create a UDP socket*/
+  int sockfd; /*file descriptor representing socket*/
+  if((sockfd = socket(domain, type, protocol)) == -1){
+    perror("SOCKET COULD NOT BE OPENED");
+    exit(EXIT_FAILURE);
+  }
+
+  /*Initialize socket*/
+  microtcp_sock_t microtcp_socket;
+  microtcp_socket.sd = sockfd;
+  microtcp_socket.state = CLOSED;
+  microtcp_socket.init_win_size = MICROTCP_WIN_SIZE;
+  microtcp_socket.curr_win_size = MICROTCP_WIN_SIZE;
+  microtcp_socket.recvbuf = (uint8_t*) malloc(MICROTCP_RECVBUF_LEN * sizeof(uint8_t));
+  microtcp_socket.buff_fill_level = 0;
+  microtcp_socket.cwnd = MICROTCP_INIT_CWND;
+  microtcp_socket.ssthresh = MICROTCP_INIT_SSTHRESH;
+  microtcp_socket.seq_number = rand();
+  microtcp_socket.ack_number = 0;
+  microtcp_socket.packets_send = 0;
+  microtcp_socket.packets_received = 0;
+  microtcp_socket.packets_lost = 0;
+  microtcp_socket.bytes_send = 0;
+  microtcp_socket.bytes_received = 0;
+  microtcp_socket.bytes_lost = 0;
+
+  return microtcp_socket;
 }
 
 int
@@ -38,7 +64,72 @@ int
 microtcp_connect (microtcp_sock_t *socket, const struct sockaddr *address,
                   socklen_t address_len)
 {
-  /* Your code here */
+  /*first check if args are valid*/
+  if(!socket || !address || address_len <= 0){
+    return -1;
+  }
+
+  if(socket->state != CLOSED){
+    return -1; /*socket must be in CLOSED state*/
+  }
+
+  /*create header to send SYN to the server*/
+  microtcp_header_t header;
+  header.seq_number = socket->seq_number;
+  header.ack_number = 0;
+  header.control = 0x2;  //SYN bit
+  header.window = socket->curr_win_size;
+  header.data_len = 0;
+  header.future_use0 = 0;
+  header.future_use1 = 0;
+  header.future_use2 = 0;
+  header.checksum = 0;
+  header.checksum = crc32((uint8_t*) &header, sizeof(microtcp_header_t));
+
+  /*send SYN to server*/
+  if(sendto(socket->sd, &header, sizeof(microtcp_header_t), 0, address, address_len) == -1){ /*0 payload so data len is header len*/
+    perror("SYN could not be sent");
+    return -1;
+  }
+
+  /*wait for SYN-ACK from server(recvfrom blocks the function until it receives data so no need to block ourselves until data arrives)*/
+  microtcp_header_t header_received;
+  struct sockaddr_in *sender_address;
+  socklen_t sender_address_len = sizeof(struct sockaddr_in);
+  ssize_t bytes = recvfrom(socket->sd, &header_received, sizeof(microtcp_header_t), 0, &sender_address, &sender_address_len);
+
+
+  if(bytes == -1){
+    perror("SYN-ACK failed");
+    return -1;
+  }
+
+  /*check if control bits match SYN-ACK*/
+  if(header_received.control != 0x12){
+    perror("SYN-ACK not received");
+    return -1;
+  }
+  /*update ack seq_num and check for errors in checksum*/
+  uint32_t received_checksum = header_received.checksum;
+  header_received.checksum = 0;
+  uint32_t calculated_checksum = crc32((uint8_t*) &header_received, sizeof(microtcp_header_t));
+  if(received_checksum != calculated_checksum){
+    perror("Error in checksum of SYN-ACK");
+    return -1;
+  }
+
+  socket->ack_number = header_received.seq_number + 1;
+  socket->seq_number += 1;
+  header.seq_number = socket->seq_number;
+  header.ack_number = socket->ack_number;
+
+  /*received SYN-ACK so client sends ACK*/
+  if(sendto(socket->sd, &header, sizeof(microtcp_header_t), 0, address, address_len) == -1){ /*0 payload so data len is header len*/
+    perror("ACK could not be sent");
+    return -1;
+  }
+  socket->state = ESTABLISHED;
+  return 0;
 }
 
 int
